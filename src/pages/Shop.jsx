c47b9c7f110fe820f84940c8c0b007e8f24ad0eb
sejansorker <sejansorker@gmail.com>
2026-08-08 05:06:21 +0600

    shop page start

--- src/pages/Shop.jsx
+++ src/pages/Shop.jsx
@@ -1,9 +1,25 @@
-import React from 'react'
-
+import React, { useContext } from 'react'
+import { ProductApi } from '../components/ContextApi'
+import Container from '../components/Container'
+import { CiHeart, CiShoppingCart } from 'react-icons/ci'
+import { FaSearchPlus } from 'react-icons/fa'
+import One from "../assets/one.png"
 const Shop = () => {
+  const data = useContext(ProductApi)
   return (
-    <div>Shop
-       
+    <div>
+      <Container>
+        <div className="">
+           <div className="w-[31%]">
+            <div className="bg-[#F6F7FB] flex justify-center items-center py-13">
+              <img src={One} className='w-50 h-50' alt="" />
+            </div>
+            <div className="">
+                
+              </div>
+           </div>
+        </div>
+      </Container>
     </div>
   )
 }
