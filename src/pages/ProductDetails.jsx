3fa2535662d5947f9c58398ef02a25650b0700fc
sejansorker <sejansorker@gmail.com>
2026-08-14 19:34:34 +0600

    products details add

--- src/pages/ProductDetails.jsx
+++ src/pages/ProductDetails.jsx
@@ -1,6 +1,9 @@
 import React from 'react'
 import Container from '../components/Container'
-
+import Details from "../assets/frame.png"
+import Details1 from "../assets/frame1.png"
+import Details2 from "../assets/frame2.png"
+import { FaStar } from 'react-icons/fa'
 const ProductDetails = () => {
   return (
     <div className='pt-24 pb-30 bg-[#F6F5FF]'>
@@ -8,6 +11,33 @@ const ProductDetails = () => {
                 <div className="">
                     <h2 className='text-[36px] text-[#101750] font-main font-bold pb-2'>Product Details</h2>
                     <p>Home.Pages.<span className='text-[#FB2E86] text-16px] font-main font-medium'>Product Details</span></p>
+                </div>
+                <div className="py-30 ">
+                   <div className="flex py-3">
+                    <div className="w-1/2 flex gap-x-3">
+                    <div className="w-[155px] ml-3">
+                        <img className='h-[155px]' src={Details} alt="" />
+                        <img className='h-[155px]' className='py-3' src={Details1} alt="" />
+                        <img className='h-[155px]' src={Details2} alt="" />
+                    </div>
+                    <div className="">
+                        <img className='w-[400px] h-[442px]' src={Details} alt="" />
+                    </div>
+                    </div>
+                   <div className="w-1/2">
+                   <div className="">
+                    <h4>Playwood arm chair</h4>
+                    <i className='flex items-center'><FaStar/><FaStar/><FaStar/><FaStar/> <FaStar/></i>
+                    <p>$32.00</p>
+                    <h6>Color</h6>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris tellus porttitor purus, et volutpat sit.</p>
+                    <button>Add To cart</button>
+                    <h5>Categories:</h5>
+                    <h6>Tags</h6>
+                    <p>Share</p>
+                   </div>
+                   </div>
+                   </div>
                 </div>
             </Container>
         </div>
